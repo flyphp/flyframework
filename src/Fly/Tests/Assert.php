c91b0d1541e3212ce92f2bbc10a3c586eb2c6ac1
Allan Freitas <allanfreitasci@gmail.com>
2014-02-14 15:48:30 -0200

    Add Tests Helpers Module

---
+++ src/Fly/Tests/Assert.php
@@ -0,0 +1,25 @@
+<?php namespace Fly\Tests;
+
+require_once 'TestFacade.php';
+
+class Assert extends TestFacade {
+    
+    protected $aliases = array(
+        'eq'       => 'assertEquals',
+        'has'      => 'assertContains',
+        'type'     => 'assertInternalType',
+        'instance' => 'assertInstanceOf'
+    );
+
+    protected function getMethod($methodName)
+    {
+        // If an alias is registered,
+        // that takes precendence.
+        if ($this->isAnAlias($methodName))
+        {
+            return $this->aliases[$methodName];
+        }
+
+        return 'assert' . ucwords($methodName);
+    }
+}
